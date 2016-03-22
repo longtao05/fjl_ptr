ee38ccd94a59838567dcd8ec3480e3e666a0b870
eason <eason@bogon.localdomain>
2016-03-22 17:45:17 +0800

    first commit

--- ptr.cpp
+++ ptr.cpp
@@ -1,10 +1,14 @@
 /*************************************************************************
-	> File Name: delete.cpp
+	> File Name: ptr.cpp
 	> Author: fjl_57
 	> Mail: 799619622@qq..com 
-	> Created Time: Tue 22 Mar 2016 05:42:02 PM CST
+	> Created Time: Tue 22 Mar 2016 05:44:07 PM CST
  ************************************************************************/
 
 #include<iostream>
 using namespace std;
-
+int main()
+{
+	cout<<"success"<<endl;
+	return 0;
+}
